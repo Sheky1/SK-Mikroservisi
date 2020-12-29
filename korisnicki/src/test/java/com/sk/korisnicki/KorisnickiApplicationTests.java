afa5bc5e38815c450f8cf49fa4eaa406557565b7
Dimitrije <dsejat@gmail.com>
2020-12-29 16:43:03 +0100

    asd

---
+++ korisnicki/src/test/java/com/sk/korisnicki/KorisnickiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sk.korisnicki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KorisnickiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
